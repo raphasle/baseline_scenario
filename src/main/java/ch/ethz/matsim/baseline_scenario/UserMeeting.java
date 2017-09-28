655551c181cb4dca3d319dfff05f352944489cc3
Sebastian Hörl <hoerl.sebastian@gmail.com>
2017-09-28 20:27:28 +0200

    making car worse

--- src/main/java/ch/ethz/matsim/baseline_scenario/UserMeeting.java
+++ src/main/java/ch/ethz/matsim/baseline_scenario/UserMeeting.java
@@ -95,7 +95,7 @@ public class UserMeeting {
 				ModeChoiceMNL model = new ModeChoiceMNL(MatsimRandom.getRandom(), chainAlternatives, network,
 						useBestResponse ? ModeChoiceMNL.Mode.BEST_RESPONSE : ModeChoiceMNL.Mode.SAMPLING, prior);
 
-				BasicModeChoiceParameters carParameters = new BasicModeChoiceParameters(0.0, -0.2 / 1000.0,
+				BasicModeChoiceParameters carParameters = new BasicModeChoiceParameters(-4.0, -0.2 / 1000.0,
 						-23.29 / 3600.0, true);
 				BasicModeChoiceParameters ptParameters = new BasicModeChoiceParameters(0.0, -0.5 / 1000.0,
 						-14.43 / 3600.0, false);
@@ -184,7 +184,7 @@ public class UserMeeting {
 		config.planCalcScore().setMarginalUtlOfWaitingPt_utils_hr(0.0);
 
 		ModeParams carParameters = new ModeParams("car");
-		carParameters.setConstant(0.0);
+		carParameters.setConstant(-4.0);
 		carParameters.setMarginalUtilityOfTraveling(-23.29);
 		carParameters.setMonetaryDistanceRate(-0.2 / 1000.0);
 
